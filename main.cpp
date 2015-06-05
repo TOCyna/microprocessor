e84f01207c3a7acc10f17eb16ba2e15de1b32127
Tay <taynaran.n@gmail.com>
2015-06-05 16:43:13 -0300

    v1.3

--- main.cpp
+++ main.cpp
@@ -22,38 +22,8 @@ int main()
     //  Binary bin("101010101010");
     Memory memory;
     boot->load("/opt/temp/assembly.txt", memory);
-    //cout << "Test memory: " << memory.get(1).toStr() << endl;
-    //Memory *memory = new Memory;
-    //memory->set(10,bin);
-    //cout << memory->get(100).toStr() << endl;
-    //cout << "Test get Word " << memory.getWord(3).toStr() << endl;
-    Pc pc = Pc();
-    //cout << "Word: " << pc.get().toStr() << endl;
-    for (int i = 0; i < 10; i++)
-        pc.increment();
-    Reg reg1, reg2;
-    reg1.set(Word("00000111"));
-    reg2.set(Word("11100000"));
-    //Word word = Word("00000101");
-    //cout << regs.get().toStr() << endl;
-    ULA ula;
-    //reg1.set(ula.add(reg1,reg2).get());
-    //cout << "ADD TEST: " << reg1.get().toStr() << endl;
-    //reg1.set(ula.sub(reg1,reg2).get());
-    //cout << "SUB TEST: " << reg1.get().toStr() << endl;
-    //reg1.set(ula.addi(reg1,word).get());
-    //cout << "ADDI TEST: " << reg1.get().toStr() << endl;
-    //reg1.set(ula.and_(reg1,reg2).get());
-    //cout << "AND TEST: " << reg1.get().toStr() << endl;
-    //reg1.set(ula.or_(reg1,reg2).get());
-    //cout << "OR TEST: " << reg1.get().toStr() << endl;
     Cu cu;
     cu.run(memory);
-    //Memory memory1;
-    //Word word = Word("00001111");
-    //memory1.set(0, word);
-    //cout << memory1.get(0).toStr();
-    cout << "=== END MAIN ===\n" ;
     return 0;
 }
 
